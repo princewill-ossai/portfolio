cf4830451dff6bd5bc98679b85c777b367ae7c06
oreoluwa ogundimu <oreoluwaogundimu19@gmail.com>
2024-07-04 10:41:54 +0100

    sidebar

---
+++ home/script2.js
@@ -0,0 +1,10 @@
+const toggleButton = document.getElementById('toggle-sidebar');
+const sidebar = document.getElementById('sidebar');
+
+
+
+// toggleButton.addEventListener('click', () => {
+//         sidebar.style.display = sidebar.style.display > 'none' ? 'block' : 'none';
+// //     sideBar.classList.toggle('active');
+        
+// })
